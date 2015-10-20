1b820efc94e92770af83f54bbfd118b3c31d63e0
wqhvstf <471326375@qq.com>
2015-10-20 18:08:11 +0800

    修改日志配置

--- src/main/java/com/springmvc/controller/ArticleController.java
+++ src/main/java/com/springmvc/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.springmvc.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,13 @@ import com.springmvc.service.ArticleService;
 public class ArticleController {
 
 	@Autowired
-	ArticleService articleService;
+	private ArticleService articleService;
+
+	private static final Logger LOGGER = Logger.getLogger(ArticleController.class);
 
 	@RequestMapping("/list")
 	public ModelAndView list() {
+		LOGGER.info("Start list");
 		PageHelper.startPage(1, 2);
 		List<Article> list = articleService.selectAll();
 		ModelAndView mav = new ModelAndView("hello");
